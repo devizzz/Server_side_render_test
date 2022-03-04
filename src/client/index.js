3f2f1dd08a63f015542cab88d34d5662b1d8a471
david alejandro cordoba <cordob0075@gmail.com>
2022-03-03 21:29:43 -0500

    .

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/app';
+
+const app = <App items={window.__data__}/>;
+
+ReactDom.hydrate(app, document.getElementById('root'));
